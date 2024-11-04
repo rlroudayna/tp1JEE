0b94f6664e101caf28d99b8fba0c859eda72232c
rlroudayna <louatami.roudayna.2002@gmail.com>
2024-11-04 13:50:25 +0100

    first commit

---
+++ src/main/java/com/example/tp1jee/Tp1jeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tp1jee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1jeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1jeeApplication.class, args);
+	}
+
+}
